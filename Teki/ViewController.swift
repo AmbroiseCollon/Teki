133db74fd8ad1ad416015f8d4ee28fadff9c5e03
AmbroiseCollon <ambroise.collon@gmail.com>
2017-01-04 16:31:57 +0100

    First commit

---
+++ Teki/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Teki
+//
+//  Created by Ambroise Collon OC on 04/01/2017.
+//  Copyright © 2017 Ambroise Collon OC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
