695c108c707697f530714c9204e593e714322fb6
AmbroiseCollon <ambroise.collon@gmail.com>
2017-01-11 11:15:17 +0100

    Final version

--- Teki/ViewController.swift
+++ Teki/ViewController.swift
@@ -9,11 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let celebrities = ["le Steve Jobs", "le Zinedine Zidane", "la Madonna", "le Karl Lagarfeld", "la Scarlett Johansson"]
+    let activities = ["du dancefloor", "du barbecue", "de la surprise ratée", "des blagues lourdes", "de la raclette party"]
     
     @IBOutlet weak var quoteLabel: UILabel!
 
     @IBAction func changeQuote() {
-        print("Youpi ! Notre action fonctionne parfaitement !")
+        // On séléctionne un élément alétoire parmi les célébrités
+        let randomIndex1 = Int(arc4random_uniform(UInt32(celebrities.count)))
+        let celebrity = celebrities[randomIndex1]
+        
+        // On séléctionne un élément aléatoire parmi les activités
+        let randomIndex2 = Int(arc4random_uniform(UInt32(activities.count)))
+        let activity = activities[randomIndex2]
+        
+        let quote = "Tu es " + celebrity + " " + activity + " !"
+        quoteLabel.text = quote
     }
 }
 
